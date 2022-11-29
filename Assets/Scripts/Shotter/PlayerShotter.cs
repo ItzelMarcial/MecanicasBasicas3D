1a3ecd0b5a0cddf34f20904b01201748eed7d5c6
ItzelMarcial <itzel.marcialno@alumno.buap.mx>
2022-11-29 09:50:43 -0600

    Barra de Concentracion uwu

--- Assets/Scripts/Shotter/PlayerShotter.cs
+++ Assets/Scripts/Shotter/PlayerShotter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShotter : MonoBehaviour
 {
@@ -10,8 +11,16 @@ public class PlayerShotter : MonoBehaviour
     public GameObject MirilaApuntar;
     public float FoVApuntar = 30;
     public float FoVNormal = 60;
+
     public float PtsConcentracion = 100;
-    public float ConsumoConcentracion = 10;
+    public float ConsumoConcentracion = 10; 
+    public float RecuperacionConcentracion = 5f;
+    public float MaxPuntosConcentracion = 100f;
+    public Image BarraConcentracion;
+
+    private bool PenaltiConcenctracion = false;
+
+
     public Animator AnimadorAvatar; //Acceso al Animator del avatar
 
     private void Start()
@@ -48,20 +57,29 @@ public class PlayerShotter : MonoBehaviour
         if(Input.GetButton("Fire2")) //Alt 
         {
 
-
+            
             if (PtsConcentracion > 0)
             {
                 MirilaApuntar.SetActive(true);
                 LaCamara.fieldOfView = Mathf.Lerp(LaCamara.fieldOfView, FoVApuntar, Time.deltaTime * 5); //Propiedad de la camara
                 //El lerp es para que la mirilla apuntr aparezca lentamente
                 Time.timeScale = 0.50f;
-                PtsConcentracion -= ConsumoConcentracion * (Time.deltaTime * 2); //Aproximadamente cada 10 segundos se acaba
+                //PtsConcentracion -= ConsumoConcentracion * (Time.deltaTime * 2); //Aproximadamente cada 10 segundos se acaba
+
+                PtsConcentracion = PtsConcentracion - ConsumoConcentracion * (Time.deltaTime / Time.timeScale);
+
+                
+
             }
             else
             {
                 MirilaApuntar.SetActive(false);
                 LaCamara.fieldOfView = FoVNormal;
                 Time.timeScale = 1f; //Volvemnos a la velocidad normal?
+                if (PtsConcentracion <= 0)
+                {
+                    PenaltiConcenctracion = true;
+                }
             }
 
         }
@@ -70,6 +88,24 @@ public class PlayerShotter : MonoBehaviour
             MirilaApuntar.SetActive(false);
             LaCamara.fieldOfView = FoVNormal;
             Time.timeScale = 1f; //Volvemnos a la velocidad normal?
+
+            if(PtsConcentracion < MaxPuntosConcentracion)
+            {
+                PtsConcentracion = Mathf.Clamp(PtsConcentracion + RecuperacionConcentracion * Time.deltaTime / Time.timeScale, 0, MaxPuntosConcentracion);
+            }
+            if(PtsConcentracion >= MaxPuntosConcentracion)
+            {
+                PenaltiConcenctracion = false;
+            }
+        }
+
+        BarraConcentracion.fillAmount = 
+            PtsConcentracion / MaxPuntosConcentracion;
+        BarraConcentracion.color = PenaltiConcenctracion ? Color.red : Color.blue;
+
+       if(PtsConcentracion / MaxPuntosConcentracion >= 0.5)
+        {
+            BarraConcentracion.color = Color.blue;
         }
     }
 }
